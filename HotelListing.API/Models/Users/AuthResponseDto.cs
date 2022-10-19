358a984bab76552c59a299988508a0f9114be4cf
koray tug <koraytug@hotmail.com>
2022-10-19 14:53:31 +0300

    implement refresh tokens

--- HotelListing.API/Models/Users/AuthResponseDto.cs
+++ HotelListing.API/Models/Users/AuthResponseDto.cs
@@ -3,6 +3,7 @@
     public class AuthResponseDto
     {
         public string UserId { get; set; }   
-        public string Token { get; set; }   
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
     }
 }
